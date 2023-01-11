9cbebc17b092fa55f985c9c0e373b004082795bf
Eptick <redzicleon@gmail.com>
2023-01-11 22:30:56 +0100

    Initial commit

---
+++ app/src/main/java/digital/gorilly/hiljadarka/CovertDirection.java
@@ -0,0 +1,6 @@
+package digital.gorilly.hiljadarka;
+
+public enum CovertDirection {
+    TISUCE_U_HILJADE,
+    HILJADE_U_TISUCE
+}
---
+++ app/src/main/java/digital/gorilly/hiljadarka/MainActivity.java
@@ -0,0 +1,84 @@
+package digital.gorilly.hiljadarka;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import digital.gorilly.hiljadarka.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Drawable background = binding.tisuca.getBackground();
+        if (background instanceof GradientDrawable) {
+            ((GradientDrawable)background).setColor(
+                    getResources().getColor(R.color.secondary_one));
+        }
+
+        binding.inputHiljada.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (binding.inputHiljada.hasFocus()) {
+                    binding.inputTisuca.setText(editable.toString());
+                }
+            }
+        });
+
+        binding.inputTisuca.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (binding.inputTisuca.hasFocus()) {
+                    binding.inputHiljada.setText(editable.toString());
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
